e7aa5824d420b76942a328cba1544912747ae3a6
Ernest Phillips <ernest-phillips@github.com>
2022-03-22 23:03:42 +0000

    Generate controllers index show new edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'dogs/index'
+  get 'dogs/show'
+  get 'dogs/new'
+  get 'dogs/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
